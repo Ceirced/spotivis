7ea9b195188c743567361e30acd0749f53ad5d17
Cederic Demoulin <cdemoulin@protonmail.com>
2024-10-04 16:35:03 +0200

    removed old styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,65 +2,16 @@
 
 const defaultTheme = require('tailwindcss/defaultTheme');
 
-let colors =
-  'red|yellow|green|blue|indigo|purple|pink|rose|violet|green|orange|gray|stone|black';
 module.exports = {
   content: [
     './app/templates/**/*.html',
     './app/static/**/*.js',
     './app/static/**/*.svg',
   ],
-  safelist: [
-    {
-      pattern: /row-start-+/, // needed for the year grid since the start of the year is not static
-    },
-    {
-      pattern: new RegExp(`bg-(${colors})-500`),
-    },
-    {
-      pattern: new RegExp(`border-(${colors})-(200|300|500)`),
-    },
-    {
-      pattern: new RegExp(`border-(${colors})-(600)`),
-      variants: ['hover'],
-    },
-    {
-      pattern: new RegExp(`text-(${colors})-(600)`),
-      variants: ['hover'],
-    },
-    {
-      pattern: new RegExp(`text-(${colors})-(200|500)`),
-    },
-    {
-      pattern: new RegExp(`(from)-(${colors})-(300|500)`),
-    },
-    {
-      pattern: new RegExp(`to-(${colors})-(300|500)`),
-    },
-  ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-      spacing: {
-        'grid-gap': '8px',
-        'grid-gap-lg': '20px',
-      },
-      colors: {
-        primary: {
-          50: '#f8fafc',
-          100: '#f1f5f9',
-          200: '#e2e8f0',
-          300: '#cbd5e1',
-          400: '#94a3b8',
-          500: '#64748b',
-          600: '#475569',
-          700: '#334155',
-          800: '#1e293b',
-          900: '#0f172a',
-          950: '#020617',
-        },
       },
     },
   },
