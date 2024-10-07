f00be6988785c1088a9f6abffa72eda40571ef40
Cederic Demoulin <cdemoulin@protonmail.com>
2024-10-07 13:47:32 +0200

    adapted colors for light and dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,16 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        background: {
+          light: '#f4f4f5',
+          dark: '#000000'
+        },
+        text: {
+          light: '#000000',
+          dark: '#ffffff'
+        },
+      },
     },
   },
   plugins: [require('daisyui')],
