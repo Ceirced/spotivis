adc66ae731417ffc3c9e66e4a8e2317040ec1d72
Cederic Demoulin <cdemoulin@protonmail.com>
2024-10-27 20:32:34 +0100

    also starting dev container from boot script  and upgrading db on boot closes #3

--- boot.sh
+++ boot.sh
@@ -3,11 +3,12 @@
 
 # Function to start the Flask server
 start_server() {
-    echo "Starting Flask server..."
-    if [ "$APP_SETTINGS" = "config.DevelopmentConfig" ]; then
-        exec gunicorn --bind :5000 --access-logfile - --error-logfile - template_app:app --workers 4 --reload --log-level debug
+    if [ "$APP_SETTINGS" = "config.ProductionConfig" ]; then
+        echo "Starting Flask server in production..."
+        exec gunicorn --bind :5000 --access-logfile - --error-logfile - template_app_app:app --workers 4
     else
-        exec gunicorn --bind :5000 --access-logfile - --error-logfile - template_app:app --workers 4
+        echo "Starting Flask server in dev..."
+        python3 template_app.py
     fi
 }
 
@@ -16,6 +17,16 @@ if [ -z "$APP_SETTINGS" ]; then
     echo "Error: APP_SETTINGS variable is not set."
     exit 1
 fi
+
+while true; do
+    flask db upgrade
+    if [[ "$?" == "0" ]]; then
+        break
+    fi
+    echo Deploy command failed, retrying in 5 secs...
+    sleep 5
+done
+
 
 # Start the server
 start_server
