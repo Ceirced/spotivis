4831dd4f9b2282b6243785e7deb95df8700c5aa8
Cederic Demoulin <cdemoulin@protonmail.com>
2025-03-25 11:16:46 +0100

    remove mariadb and replace with sqlite

--- start.sh
+++ start.sh
@@ -2,6 +2,17 @@
 
 # script to start the Flask server in development or production mode or to stop the server
 
+# check if the ./logs/app and ./instance directory exist and create it if they don't.
+# If they are not created, docker will create it with the wrong user and flask will not be able to write to it.
+if [ ! -d "./logs/app" ]; then
+    mkdir -p ./logs/app
+fi
+if [ ! -d "./instance" ]; then
+    mkdir -p ./instance
+fi
+
+
+
 if [ "$1" = 'dev' ]; then
     echo "Starting Flask server in DEV..."
     docker compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build
