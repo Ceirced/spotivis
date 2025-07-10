7b6a3adf8c42d614273a9fcb25a7fad8c05b6fde
Cederic Demoulin <cdemoulin@protonmail.com>
2025-07-10 17:45:06 +0200

    added control to the admin dashboard

--- app/extensions/admin.py
+++ app/extensions/admin.py
@@ -1,8 +1,55 @@
-from flask_admin import Admin  # type: ignore
+from flask import abort, redirect, request, url_for  # type: ignore
+from flask_security import current_user  # type: ignore
+from flask_admin import Admin, AdminIndexView  # type: ignore
 from flask_admin.contrib.sqla import ModelView  # type: ignore
+from flask_admin.theme import Bootstrap4Theme  # type: ignore
 
 from app import db
-from app.models import User, Payment
+from app.models import User, Role
+
+
+class MyAdminIndexView(AdminIndexView):
+    def is_accessible(self):
+        return (
+            current_user.is_active
+            and current_user.is_authenticated
+            and current_user.has_role("admin")
+        )
+
+    def _handle_view(self, name, **kwargs):
+        """
+        Override builtin _handle_view in order to redirect users when a view is not
+        accessible.
+        """
+        if not self.is_accessible():
+            if current_user.is_authenticated:
+                # permission denied
+                abort(403)
+            else:
+                # login
+                return redirect(url_for("security.login", next=request.url))
+
+
+class MyModelView(ModelView):
+    def is_accessible(self):
+        return (
+            current_user.is_active
+            and current_user.is_authenticated
+            and current_user.has_role("admin")
+        )
+
+    def _handle_view(self, name, **kwargs):
+        """
+        Override builtin _handle_view in order to redirect users when a view is not
+        accessible.
+        """
+        if not self.is_accessible():
+            if current_user.is_authenticated:
+                # permission denied
+                abort(403)
+            else:
+                # login
+                return redirect(url_for("security.login", next=request.url))
 
 
 def init_admin(app):
@@ -16,8 +63,13 @@ def init_admin(app):
 
     admin = Admin(
         name=app.config["APP_NAME"],
+        theme=Bootstrap4Theme(swatch="slate"),
+        index_view=MyAdminIndexView(
+            name="Dashboard",
+            url="/admin",
+        ),
     )
-    admin.add_view(ModelView(User, db.session))
-    admin.add_view(ModelView(Payment, db.session))
+    admin.add_view(MyModelView(User, db.session))
+    admin.add_view(MyModelView(Role, db.session))
 
     admin.init_app(app)
