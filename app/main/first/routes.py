3007440981c3e4a5709dcb69b8dfc9bbe3ae55ab
Cederic Demoulin <cdemoulin@protonmail.com>
2025-07-25 13:17:28 +0200

    only swap #content and top bar with htmx in first page

--- app/main/first/routes.py
+++ app/main/first/routes.py
@@ -1,7 +1,12 @@
 from flask import render_template
+
+from app import htmx
 from app.main.first import bp
 
 
 @bp.route("/", methods=["GET"])
 def index():
-    return render_template("./first/index.html")
+    title = "First"
+    if htmx.boosted:
+        return render_template("./first/partials/_content.html", title=title)
+    return render_template("./first/index.html", title=title)
