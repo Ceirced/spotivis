57871aa9f72efadeede8b16aca555882807b7fa3
Cederic Demoulin <cdemoulin@protonmail.com>
2025-09-14 13:58:43 +0200

    refactor(config): remove unused config
    remove migrate config and local config
    remove duplicate env variable definition secret key and app name

--- config.py
+++ config.py
@@ -10,20 +10,10 @@ class BaseConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     LOG_TO_STDOUT = os.environ.get("LOG_TO_STDOUT")
-    APP_NAME = "Template App"
-
-
-class MigrateConfig(BaseConfig):
-    load_dotenv()
-    DEBUG = True
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    FLASK_ENV = "development"
-    SQLALCHEMY_DATABASE_URI = os.getenv("DEV_DB_URI")
 
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
-    SECRET_KEY = os.getenv("SECRET_KEY")
     FLASK_ENV = "development"
     SQLALCHEMY_DATABASE_URI = "sqlite:///dev.db"
     SERVER_NAME = os.getenv("HOST_NAME")
@@ -34,12 +24,10 @@ class TestingConfig(BaseConfig):
     TESTING = True
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = "sqlite:///"
-    SECRET_KEY = os.getenv("SECRET_KEY")
     FLASK_ENV = "testing"
 
 
 class ProductionConfig(BaseConfig):
-    SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SESSION_COOKIE_SECURE = (
         True  # does not allow cookies to be sent over an unencrypted connection
@@ -50,10 +38,3 @@ class ProductionConfig(BaseConfig):
     PREFERRED_URL_SCHEME = "https"
     FLASK_ENV = "production"
     REDIS_URL = os.getenv("REDIS_URL", "redis://localhost")
-
-
-class LocalConfig(BaseConfig):
-    load_dotenv()
-    DEBUG = True
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.getenv("LOCAL_DATABASE_URI")
