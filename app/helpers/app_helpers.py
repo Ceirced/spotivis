9fe68a0e56bac32a21b50cfe4d039953d7c17515
Cederic Demoulin <cdemoulin@protonmail.com>
2025-10-10 20:16:40 +0200

    change: better caching key and add function to delete cache for both htmx and non htmx

--- app/helpers/app_helpers.py
+++ app/helpers/app_helpers.py
@@ -1,8 +1,9 @@
 from functools import wraps
 
 from flask import abort, request
+from loguru import logger
 
-from app import htmx
+from app import cache, htmx
 
 
 def disable_route():
@@ -18,7 +19,20 @@ def disable_route():
 
 def make_cache_key_with_htmx(*args, **kwargs):
     """Create cache key that includes HTMX boosted state and request path."""
-    return f"{request.endpoint}_{htmx.boosted}_{request.view_args}"
+    cache_key = request.path + ("_htmx" if htmx.boosted else "")
+    logger.debug(f"Cache key: {cache_key}")
+
+    return cache_key
+
+
+def delete_htmx_cache(path: str):
+    """Delete the cache for the key `path` and `path_htmx`.
+
+    Args:
+        path (str): The part before the _htmx in the cache key, should be `request.path`
+    """
+    cache.delete(path)
+    cache.delete(path + "_htmx")
 
 
 if __name__ == "__main__":
